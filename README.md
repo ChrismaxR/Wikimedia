034d20e91ff677b41e1dd806978b57b5920f618a
Christian van Gaalen <ctmvangaalen@gmail.com>
2018-04-13 08:56:16 +0200

    Updating README and cleaning script

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 "# Wikimedia" 
+
+My personal crack at a Wikimedia data science assignment. See for assignment: https://github.com/wikimedia-research/Discovery-Hiring-Analyst-2016
+The data is provided in the url and I downloaded it to a local dir and set that path as working dir.  
+
+This is also the first instance I use Github to do versioncontrol.
--- Wikimedia.R
+++ Wikimedia.R
@@ -9,6 +9,7 @@ library('readr')
 library('tidyverse')
 library('lubridate')
 
+setwd("C:/Users/chris/OneDrive/Rstudio/Wikimedia datascience")
 
 # Get data ----------------------------------------------------------------
 
@@ -26,23 +27,11 @@ events_log$date <- date(events_log$timestamp)
 events_log <- events_log %>% 
   arrange(timestamp, session_id)
 
+
+# Answering the Q's -------------------------------------------------------
+
 # Q1: What is our daily overall clickthrough rate? How does it vary between the groups? ------------------------------
 ## click through rate:  the proportion of search sessions where the user clicked on one of the results displayed
-
-# test <- events_log %>% 
-#   filter(session_id == "8ee241d030e9dd7c") %>% 
-#   arrange(timestamp)
-# 
-# test2 <- events_log %>% 
-#   filter(session_id == "96539ff273a06ee7") %>% 
-#   arrange(timestamp)
-# 
-# test3 <- events_log %>% 
-#   filter(session_id == "107c7917c8c667a5") %>% 
-#   arrange(timestamp)
-# 
-# test4 <- events_log %>% 
-#   filter(action == "searchResultPage")
 
 click.through.rate <- events_log %>% 
   group_by(date, group) %>% 
@@ -51,25 +40,8 @@ click.through.rate <- events_log %>%
   ) %>% 
   mutate(rate = visit/search * 100)
 
-  
+
 ## Q2: Which results do people tend to try first? How does it change day-to-day? -----------------------------------
-
-test5 <- events_log %>% 
-  filter(action == "visitPage",
-         result_position == 1
-         ) %>% 
-  arrange(timestamp) %>% 
-  group_by(date) %>% 
-  summarise(count = n())
-
-test6 <- events_log %>% 
-  filter(action == "visitPage",
-         result_position > 1
-  ) %>% 
-  arrange(timestamp) %>% 
-  group_by(date) %>% 
-  summarise(count = n())
-
 
 result.choice <- events_log %>% 
   filter(action == "visitPage") %>% 
@@ -85,20 +57,6 @@ result.choice <- events_log %>%
 
 ## zero results rate: the proportion of searches that yielded 0 results
 
-
-
-# non.zero <- events_log %>% 
-#   filter(action == "searchResultPage",
-#          n_results > 0) %>% 
-#   group_by(date) %>% 
-#   summarise(sessions = n())
-# 
-# zero <- events_log %>% 
-#   filter(action == "searchResultPage",
-#          n_results == 0) %>% 
-#   group_by(date) %>% 
-#   summarise(sessions = n())
-
 zero.results.rate <- events_log %>% 
   filter(action == "searchResultPage") %>% 
   group_by(date, group) %>% 
@@ -108,15 +66,73 @@ zero.results.rate <- events_log %>%
   mutate(rate = zero/total * 100)
 
 
-# Data exploratie ---------------------------------------------------------
+# Data exploration ---------------------------------------------------------
 
 
-# aantal sessies per dag
+## Overall
+
+# number of sessions per day
 sessions.day <- events_log %>% 
   group_by(date) %>% 
   summarise(sessions = n())
 
-# aantal sessie per event per dag
+# number of events per day
 sessions.events.day <- events_log %>% 
   group_by(date, action) %>% 
   summarise(sessions = n())
+
+# Looking at number of individual sessions. 
+test <- events_log %>%
+  filter(session_id == "8ee241d030e9dd7c") %>%
+  arrange(timestamp)
+
+test2 <- events_log %>%
+  filter(session_id == "96539ff273a06ee7") %>%
+  arrange(timestamp)
+
+test3 <- events_log %>%
+  filter(session_id == "107c7917c8c667a5") %>%
+  arrange(timestamp)
+
+
+##Q1
+
+# Looking at how rows look where action == "searchResultPage"
+test4 <- events_log %>%
+  filter(action == "searchResultPage")
+
+
+## Q2
+
+test5 <- events_log %>% 
+  filter(action == "visitPage",
+         result_position == 1
+  ) %>% 
+  arrange(timestamp) %>% 
+  group_by(date) %>% 
+  summarise(count = n())
+
+test6 <- events_log %>% 
+  filter(action == "visitPage",
+         result_position > 1
+  ) %>% 
+  arrange(timestamp) %>% 
+  group_by(date) %>% 
+  summarise(count = n())
+
+
+## Q3
+
+# counting rows for "zero" and more than zero n-results 
+non.zero <- events_log %>%
+  filter(action == "searchResultPage",
+         n_results > 0) %>%
+  group_by(date) %>%
+  summarise(sessions = n())
+
+zero <- events_log %>%
+  filter(action == "searchResultPage",
+         n_results == 0) %>%
+  group_by(date) %>%
+  summarise(sessions = n())
+
